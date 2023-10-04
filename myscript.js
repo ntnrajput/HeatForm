747031c83dd0a48e48a4b343e0541e343d1bf8c9
ntnrajput <ntn.rajput89@gmail.com>
2023-10-03 23:12:23 -0700

    new changes

--- myscript.js
+++ myscript.js
@@ -5,18 +5,53 @@ const toleranceInputs = document.querySelectorAll('.tolerance-input');
 // Define tolerance values for each grade
 const toleranceValues = {
     R260: {
-        L_C: { min: 0.68, max: 0.80 },
-        L_Mn: { min: 0.2, max: 0.5 }
+        L_C: { min: 0.62, max: 0.80 }, L_Mn: { min: 0.7, max: 1.2 }, L_P: { min: 0, max: 0.025 },
+        L_S: { min: 0, max: 0.025 }, L_Si: { min: 0.15, max: 0.58 }, L_Al: { min: 0, max: 0.004 },
+        L_Cr: { min: 0, max: 0.15 }, L_V: { min: 0, max: 0.030 }, L_N: { min: 0, max: 0.009 },
+        H: { min: 0, max: 1.6 }, L_O: { min: 0, max: 20 }, L_Cu: { min: 0, max: 0.15 },
+        L_Ni: { min: 0, max: 0.1 }, L_Mo: { min: 0, max: 0.02 }, L_Nb: { min: 0, max: 0.04 },
+        L_Ti: { min: 0, max: 0.025 }, L_Sb: { min: 0, max: 0.02 }, L_Sn: { min: 0, max: 0.03 },
+        L_Cu10Sn: { min: 0, max: 0.35 }, L_others: { min: 0, max: 0.25 },
+        P_C: { min: 0.6, max: 0.82 }, P_Mn: { min: 0.65, max: 1.25 }, P_P: { min: 0, max: 0.025 },
+        P_S: { min: 0, max: 0.03 }, P_Si: { min: 0.13, max: 0.6 }, P_Al: { min: 0, max: 0.004 },
+        P_Cr: { min: 0, max: 0.15 }, P_V: { min: 0, max: 0.030 }, P_N: { min: 0, max: 0.010 },        
+        H: { min: 0, max: 1.6 }, P_O: { min: 0, max: 20 }, P_Cu: { min: 0, max: 0.15 },        
+        P_Ni: { min: 0, max: 0.1 }, P_Mo: { min: 0, max: 0.02 }, P_Nb: { min: 0, max: 0.04 },        
+        P_Ti: { min: 0, max: 0.025 }, P_Sb: { min: 0, max: 0.02 }, P_Sn: { min: 0, max: 0.03 },        
+        P_Cu10Sn: { min: 0, max: 0.35 }, P_others: { min: 0, max: 0.25 },
+        UTS: { min: 880, max: 10000 }, YS: { min: 550, max: 10000 }, El: { min: 10, max: 100 },
+        SH: { min: 260, max: 300},
         // Add more fields and their tolerances here
     },
     880: {
-        L_C: { min: 0.65, max: 0.75 },
-        L_Mn: { min: 0.15, max: 0.45 }
+        L_C: { min: 0.60, max: 0.80 }, L_Mn: { min: 0.8, max: 1.3 }, L_P: { min: 0, max: 0.03 },
+        L_S: { min: 0, max: 0.030 }, L_Si: { min: 0.1, max: 0.5 }, L_Al: { min: 0, max: 0.015 },
+        L_Cr: { min: 0, max: 0.30 }, L_V: { min: 0, max: 0.01 }, 
+        H: { min: 0, max: 1.6 },        
+        P_C: { min: 0.6, max: 0.8 }, P_Mn: { min: 0.8, max: 1.3 }, P_P: { min: 0, max: 0.035 },
+        P_S: { min: 0, max: 0.035 }, P_Si: { min: 0.1, max: 0.5 }, P_Al: { min: 0, max: 0.015 },
+        P_Cr: { min: 0, max: 0.30 }, P_V: { min: 0, max: 0.01 },    
+        UTS: { min: 880, max: 10000 }, YS: { min: 460, max: 10000 }, El: { min: 10, max: 100 },
+        SH: { min: 260, max: 1000},
         // Add more fields and their tolerances here
     },
     R350HT: {
-        L_C: { min: 0.70, max: 0.82 },
-        L_Mn: { min: 0.25, max: 0.55 }
+        L_C: { min: 0.72, max: 0.80 }, L_Mn: { min: 0.7, max: 1.2 }, L_P: { min: 0, max: 0.02 },
+        L_S: { min: 0, max: 0.025 }, L_Si: { min: 0.15, max: 0.58 }, L_Al: { min: 0, max: 0.004 },
+        L_Cr: { min: 0, max: 0.15 }, L_V: { min: 0, max: 0.030 }, L_N: { min: 0, max: 0.009 },
+        H: { min: 0, max: 1.6 }, L_O: { min: 0, max: 20 }, L_Cu: { min: 0, max: 0.15 },
+        L_Ni: { min: 0, max: 0.1 }, L_Mo: { min: 0, max: 0.02 }, L_Nb: { min: 0, max: 0.04 },
+        L_Ti: { min: 0, max: 0.025 }, L_Sb: { min: 0, max: 0.02 }, L_Sn: { min: 0, max: 0.03 },
+        L_Cu10Sn: { min: 0, max: 0.35 }, L_others: { min: 0, max: 0.25 },
+        P_C: { min: 0.7, max: 0.82 }, P_Mn: { min: 0.65, max: 1.25 }, P_P: { min: 0, max: 0.025 },
+        P_S: { min: 0, max: 0.03 }, P_Si: { min: 0.13, max: 0.6 }, P_Al: { min: 0, max: 0.004 },
+        P_Cr: { min: 0, max: 0.15 }, P_V: { min: 0, max: 0.030 }, P_N: { min: 0, max: 0.010 },        
+        H: { min: 0, max: 1.6 }, P_O: { min: 0, max: 20 }, P_Cu: { min: 0, max: 0.15 },        
+        P_Ni: { min: 0, max: 0.1 }, P_Mo: { min: 0, max: 0.02 }, P_Nb: { min: 0, max: 0.04 },        
+        P_Ti: { min: 0, max: 0.025 }, P_Sb: { min: 0, max: 0.02 }, P_Sn: { min: 0, max: 0.03 },        
+        P_Cu10Sn: { min: 0, max: 0.35 }, P_others: { min: 0, max: 0.25 },
+        UTS: { min: 1175, max: 10000 }, YS: { min: 560, max: 10000 }, El: { min: 9, max: 100 },
+        SH: { min: 350, max: 390},
         // Add more fields and their tolerances here
     }
 };
